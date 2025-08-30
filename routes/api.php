f35de811221aa0d1d7f86d3c0236a304c0a17179
Katherine Flores <kfloresf1@miumg.edu.gt>
2025-08-29 20:40:07 -0600

    login y register

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/test', function () {
+    return response()->json([ 'message' => 'La API funciona correctamente' ]);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
